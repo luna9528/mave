eebca03940bc95f75ddf58931f32492b52f61839
Vile11 <107072102+Vile11@users.noreply.github.com>
2022-06-07 15:47:06 -0300

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+const { Router } = require("express");
+const router = Router();
+
+
+//aqui quiero que pase un boton ver mas y me redireccione a esa routess
+router.get("/AFtoyota", (req, res) => {
+  res.render("vermas/AFtoyota");
+});
+
+router.get("/medica-tartagal", (req, res) => {
+  res.render("vermas/medica-tartagal");
+});
+
+router.get("/casaca-river", (req, res) => {
+  res.render("vermas/casaca-river");
+});
+
+router.get("/uefa-europa", (req, res) => {
+  res.render("vermas/uefa-europa");
+});
+
+
+router.get("/mia-khalifa", (req, res) => {
+  res.render("vermas/mia-khalifa");
+});
+
+
+router.get("/octavo-champions", (req, res) => {
+  res.render("vermas/octavos-champions");
+});
+
+
+router.get("/champions", (req, res) => {
+  res.render("vermas/champions");
+});
+
+
+router.get("/cr7", (req, res) => {
+  res.render("vermas/cr7");
+});
+
+
+router.get("/julian-juventud", (req, res) => {
+  res.render("vermas/julian-juventud");
+});
+
+router.get("/wichi-salta", (req, res) => {
+  res.render("vermas/wichi-salta");
+});
+
+router.get("/mas", (req, res) => {
+  res.render("pages/mas");
+});
+
+router.get("/more", (req, res) => {
+  res.render("pages/more");
+});
+
+router.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+router.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+module.exports = router;
